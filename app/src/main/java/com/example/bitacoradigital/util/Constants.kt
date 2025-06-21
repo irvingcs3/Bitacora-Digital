d9b7a87d54b8f2328786c1d0be6aec4b69a5a986
irvingcs3 <101915497+irvingcs3@users.noreply.github.com>
2025-06-21 03:38:12 -0600

    Add QR management module

--- app/src/main/java/com/example/bitacoradigital/util/Constants.kt
+++ app/src/main/java/com/example/bitacoradigital/util/Constants.kt
@@ -3,3 +3,12 @@ package com.example.bitacoradigital.util
 object Constants {
     const val FILE_PROVIDER_AUTHORITY: String = "com.example.bitacoradigital.fileprovider"
 }
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Long.toReadableDate(): String {
+    val date = Date(this)
+    val format = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+    return format.format(date)
+}
