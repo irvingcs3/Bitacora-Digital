8059137026597e9338f9814c677df5267761d52b
irvingcs3 <101915497+irvingcs3@users.noreply.github.com>
2025-06-23 23:10:39 -0600

    Fix experimental API warning and missing import

--- app/src/main/java/com/example/bitacoradigital/ui/screens/RegistroQRScreen.kt
+++ app/src/main/java/com/example/bitacoradigital/ui/screens/RegistroQRScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -50,6 +51,7 @@ import kotlinx.coroutines.launch
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegistroQRScreen(perimetroId: Int, navController: NavHostController) {
     val context = LocalContext.current
